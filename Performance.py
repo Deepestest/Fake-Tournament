9acb0cb7f142b4d618c70945d5d3073100e947d5
Deepesh Kothari <112287069+Deepestest@users.noreply.github.com>
2023-10-21 18:42:44 -0600

    added robot class

---
+++ Performance.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+
+class Robot:
+    def __init__(self, HCO, HCOV, HCU, HCUV, MCO, MCOV, MCU, MCUV, LP, LPV):
+        self.HCO = HCO
+        self.HCOV = HCOV
+        self.HCU = HCU
+        self.HCUV = HCUV
+        self.MCO = MCO
+        self.MCOV = MCOV
+        self.MCU = MCU
+        self.MCUV = MCUV
+        self.LP = LP
+        self.LPV = LPV
+
+class LowCube(Robot):
+    def __init__():
+        pass
